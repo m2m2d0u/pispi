e1c57e41d3128322b2416af683af7ca12778da72
mamadou-abass-diallo-sre <m.abassdiallo@gmail.con>
2026-03-06 14:41:00 +0000

    init project

---
+++ src/main/java/ci/sycapay/pispi/PispiApplication.java
@@ -0,0 +1,13 @@
+package ci.sycapay.pispi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PispiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PispiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/sycapay/pispi/PispiApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.sycapay.pispi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PispiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
